8f4e83983f3f2d717227faac99e7bfe407edb9eb
lsy9677-eng <lsy9677@gmail.com>
2026-04-24 10:41:44 +0900

    Add Naver OAuth login function

---
+++ functions/naver/login.js
@@ -0,0 +1,10 @@
+export async function onRequest(context) {
+  const CLIENT_ID = "YOUR_CLIENT_ID";
+  const REDIRECT_URI = "https://tennis230.pages.dev/naver/callback";
+
+  const state = Math.random().toString(36).substring(2);
+
+  const url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=${state}`;
+
+  return Response.redirect(url, 302);
+}
